258511675c89a40204469d3fb0607447479588ad
0xNazgul <87991258+0xNazgul@users.noreply.github.com>
2022-05-30 13:28:43 -0500

    Update EchidnaUtil.sol

--- EchidnaUtil.sol
+++ EchidnaUtil.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+
 import "ABDKMath64x64.sol";
 
-/// @dev Boiler Plate for all ABDKMath64x64 functions excluding the ABDKMath64x64 developer marked private functions sqrtu and divuu
+/// @title Echidna-CAREX
+/// @author 0xNazgul
+/// @notice Boiler Plate for all ABDKMath64x64 functions excluding the ABDKMath64x64 developer marked private functions sqrtu and divuu.
 contract TestUtil {
 	int128 internal zero = ABDKMath64x64.fromInt(0);
 	int128 internal one = ABDKMath64x64.fromInt(1);
@@ -113,4 +116,73 @@ contract TestUtil {
 	function exp(int128 x) public pure returns (int128) {
 		return ABDKMath64x64.exp(x);
 	}
+
+	function divuu (uint256 x, uint256 y) public pure returns (uint128) {
+		unchecked {
+		require (y != 0);
+
+		uint256 result;
+
+		if (x <= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
+			result = (x << 64) / y;
+		else {
+			uint256 msb = 192;
+			uint256 xc = x >> 192;
+			if (xc >= 0x100000000) { xc >>= 32; msb += 32; }
+			if (xc >= 0x10000) { xc >>= 16; msb += 16; }
+			if (xc >= 0x100) { xc >>= 8; msb += 8; }
+			if (xc >= 0x10) { xc >>= 4; msb += 4; }
+			if (xc >= 0x4) { xc >>= 2; msb += 2; }
+			if (xc >= 0x2) msb += 1;  // No need to shift xc anymore
+
+			result = (x << 255 - msb) / ((y - 1 >> msb - 191) + 1);
+			require (result <= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
+
+			uint256 hi = result * (y >> 128);
+			uint256 lo = result * (y & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
+
+			uint256 xh = x >> 192;
+			uint256 xl = x << 64;
+
+			if (xl < lo) xh -= 1;
+			xl -= lo; // We rely on overflow behavior here
+			lo = hi << 128;
+			if (xl < lo) xh -= 1;
+			xl -= lo; // We rely on overflow behavior here
+
+			require (xh == hi >> 128);
+
+			result += xl / y;
+		}
+
+		require (result <= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
+		return uint128 (result);
+		}
+	}
+
+	function sqrtu (uint256 x) public pure returns (uint128) {
+		unchecked {
+			if (x == 0) return 0;
+			else {
+				uint256 xx = x;
+				uint256 r = 1;
+				if (xx >= 0x100000000000000000000000000000000) { xx >>= 128; r <<= 64; }
+				if (xx >= 0x10000000000000000) { xx >>= 64; r <<= 32; }
+				if (xx >= 0x100000000) { xx >>= 32; r <<= 16; }
+				if (xx >= 0x10000) { xx >>= 16; r <<= 8; }
+				if (xx >= 0x100) { xx >>= 8; r <<= 4; }
+				if (xx >= 0x10) { xx >>= 4; r <<= 2; }
+				if (xx >= 0x8) { r <<= 1; }
+				r = (r + x / r) >> 1;
+				r = (r + x / r) >> 1;
+				r = (r + x / r) >> 1;
+				r = (r + x / r) >> 1;
+				r = (r + x / r) >> 1;
+				r = (r + x / r) >> 1;
+				r = (r + x / r) >> 1; // Seven iterations should be enough
+				uint256 r1 = x / r;
+				return uint128 (r < r1 ? r : r1);
+			}
+		}
+	}	
 }
